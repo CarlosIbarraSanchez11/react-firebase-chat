8bdfd29bdfafde5c2336f65721e3f43fc0826270
Carlos - San Pablo <intranet@istsanpablo.edu.pe>
2024-05-15 17:15:59 -0500

    before go at home

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useRef, useState } from "react"
 import "./chat.css"
-// import {emoji} from "../../../public/emoji.png"
+import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const endRef = useRef(null)
+  useEffect(()=>{
+    endRef.current?.scrollIntoView({behavior:"smooth"})
+  }, [])
+
+  const handleEmoji = e =>{
+    // console.log(e)
+    setText(prev => prev+e.emoji)
+    setOpen(false)
+  }
+  
+  // console.log(text)
   return (
     <div className="chat">
       <div className="top">
@@ -18,16 +34,48 @@ const Chat = () => {
           <img src="./info.png"/>
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png"/>
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nisi illo quisquam ad eius earum tenetur? Impedit harum tenetur pariatur autem ea amet praesentium, odit reprehenderit labore. Maiores, aliquid molestiae.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nisi illo quisquam ad eius earum tenetur? Impedit harum tenetur pariatur autem ea amet praesentium, odit reprehenderit labore. Maiores, aliquid molestiae.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png"/>
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nisi illo quisquam ad eius earum tenetur? Impedit harum tenetur pariatur autem ea amet praesentium, odit reprehenderit labore. Maiores, aliquid molestiae.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://i.ibb.co/6bXqFcM/slide-01-removebg-preview.png" alt=""/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nisi illo quisquam ad eius earum tenetur? Impedit harum tenetur pariatur autem ea amet praesentium, odit reprehenderit labore. Maiores, aliquid molestiae.</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div ref={endRef}></div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png"/>
           <img src="./camera.png"/>
           <img src="./mic.png"/>  
         </div>
-        <input type="text" placeholder="Type a message" />
+        <input type="text" value={text} placeholder="Type a message" onChange={e => setText(e.target.value)}/>
         <div className="emoji">
-          <img src="./emoji.png"/>
+          <img src="./emoji.png" onClick={()=>setOpen(prev => !prev)}/>
+          <div className="picker">
+            <EmojiPicker open ={open} onEmojiClick={handleEmoji}/>
+          </div>
         </div>
         <button className="sendButton">Send</button>
       </div>
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,7 +3,49 @@ import "./detail.css"
 const Detail = () => {
   return (
     <div className="detail">
-      detail
+      <div className="user">
+        <img src="./avatar.png"/>
+        <h2>
+          Jane Does
+        </h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title"> 
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png"/>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title"> 
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png"/>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title"> 
+            <span>Shared photos</span>
+            <img src="./arrowDown.png"/>
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://i.ibb.co/6bXqFcM/slide-01-removebg-preview.png"/>
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" className="icon"></img>
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title"> 
+            <span>Shared files</span>
+            <img src="./arrowUp.png"/>
+          </div>
+        </div>
+        <button className="block-user">Block user</button>
+      </div>
     </div>
   )
 }
