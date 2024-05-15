6854d8bb6b6a07efcee37e8c85b029ad686a25ce
Carlos - San Pablo <intranet@istsanpablo.edu.pe>
2024-05-15 11:35:10 -0500

    adding new components and styles

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,36 @@
 import "./chat.css"
+// import {emoji} from "../../../public/emoji.png"
 
 const Chat = () => {
   return (
     <div className="chat">
-      chat
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png"/>
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png"/>
+          <img src="./video.png"/>
+          <img src="./info.png"/>
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png"/>
+          <img src="./camera.png"/>
+          <img src="./mic.png"/>  
+        </div>
+        <input type="text" placeholder="Type a message" />
+        <div className="emoji">
+          <img src="./emoji.png"/>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
     </div>
   )
 }
